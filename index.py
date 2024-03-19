c0d6ba916460a2cb4fe80b358dbadcb2cef1bbf8
@mmogesdesigns <mmogesdesigns@gmail.com>
2024-03-19 08:05:10 -0700

    intial commit

---
+++ index.py
@@ -0,0 +1,29 @@
+weather == "sunny"
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+    #fixed indentation 
+how_do_you_feel =input("How do you feel today?")
+if input == "happy":
+    print("Happy")
+else:
+    print("sad")
+
+# Pi_value = <span class="hljs-number">3.14</span>
+# userAge = <span class="hljs-number">25</span>
+# user_location = <span class="hljs-string">"New York"</span>
+# MAXLIMIT = <span class="hljs-number">1000</span>
+PiValue = 3.14
+UserAge = 25
+Userocation = "New York"
+MaxLimit = 1000
+
+variable_a = "Hello, World!"
+print(type(variable_a))
+variable_b = 23
+print(type(variable_b))
+variable_c = 3.14
+print(type(variable_c))
+variable_d = True
+print(type(variable_d))
